44a7a2a1d4311e9bc247675ff21287e8d3146053
Rat <ratulbhowmick65@gmail.com>
2021-08-16 03:10:20 +0600

    download invoice

--- src/views/apps/invoice/preview/PreviewActions.js
+++ src/views/apps/invoice/preview/PreviewActions.js
@@ -1,39 +1,73 @@
 // ** React Imports
-import { Link } from 'react-router-dom'
+/*eslint-disable*/
+import { Link } from "react-router-dom";
 
 // ** Third Party Components
-import { Card, CardBody, Button } from 'reactstrap'
+import Pdf from "react-to-pdf";
+import { Card, CardBody, Button } from "reactstrap";
 
-const PreviewActions = ({ id, setSendSidebarOpen, setAddPaymentOpen }) => {
+const PreviewActions = ({
+  id,
+  setSendSidebarOpen,
+  setAddPaymentOpen,
+  pdfRef,
+}) => {
   return (
-    <Card className='invoice-action-wrapper'>
+    <Card className="invoice-action-wrapper">
       <CardBody>
-        <Button.Ripple color='primary' block className='mb-75' onClick={() => setSendSidebarOpen(true)}>
+        <Button.Ripple
+          color="primary"
+          block
+          className="mb-75"
+          onClick={() => setSendSidebarOpen(true)}
+        >
           Send Invoice
         </Button.Ripple>
-        <Button.Ripple color='secondary' block outline className='mb-75'>
-          Download
-        </Button.Ripple>
+        <Pdf targetRef={pdfRef} filename={`Invoice-${id}`}>
+          {({ toPdf }) => (
+            <Button.Ripple
+              color="secondary"
+              block
+              outline
+              className="mb-75"
+              onClick={toPdf}
+            >
+              Download
+            </Button.Ripple>
+          )}
+        </Pdf>
+
         <Button.Ripple
-          color='secondary'
+          color="secondary"
           tag={Link}
-          to='/apps/invoice/print'
-          target='_blank'
+          to="/apps/invoice/print"
+          target="_blank"
           block
           outline
-          className='mb-75'
+          className="mb-75"
         >
           Print
         </Button.Ripple>
-        <Button.Ripple tag={Link} to={`/apps/invoice/edit/${id}`} color='secondary' block outline className='mb-75'>
+        <Button.Ripple
+          tag={Link}
+          to={`/apps/invoice/edit/${id}`}
+          color="secondary"
+          block
+          outline
+          className="mb-75"
+        >
           Edit
         </Button.Ripple>
-        <Button.Ripple color='success' block onClick={() => setAddPaymentOpen(true)}>
+        <Button.Ripple
+          color="success"
+          block
+          onClick={() => setAddPaymentOpen(true)}
+        >
           Add Payment
         </Button.Ripple>
       </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default PreviewActions
+export default PreviewActions;
